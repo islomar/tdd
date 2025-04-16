e4ed3f32dada51942adfa0f518bbf45c05614ce2
Isidro Lopez <islomar@gmail.com>
2025-04-16 12:47:25 +0200

    Update Writing maintainable tests exercise_1

--- exercises/writing_maintainable_tests/exercise_1/test-utils.js
+++ exercises/writing_maintainable_tests/exercise_1/test-utils.js
@@ -1,3 +1,37 @@
 export const testUsers = [
   // pretend there's a large array of test user data here
 ];
+
+// 1. What code from the tests might you factor out into reusable functions?
+const mockDao = aMockDaoReturningUsers(testUsers)
+
+// Abstract HTTP Response: maybe too much? (KISS)
+isResponseStatus(httpStatus.OK)
+isContentType(APPLICATION_JSON)
+assertHttpResponseContainsJSonAndOkStatus()
+
+// Payload structure
+assertResponsePayload(payload, expectedData, expectedNextPage)
+
+
+// 2. What have you made easier or nicer by doing this? What problems might you have created?
+/**
+- Nicer
+    - Less code to understand at a higher level
+    - Lower cognitive load when you need it somewhere else (you start typing…): higher level of abstraction, you abstract the “lower level details”, e.g. xx
+    - You focus on what it matters for the test
+        - E.g. ignore the “previousPage”
+
+- Not so nice
+    - One more level of indirection
+    - Increased abstraction: more difficult to reason, to understand
+    - Changes or an error in those extracted functions could potentially affect multiple tests.
+        - If the test cases grow, we might try to continuously adapt those helper functions, becoming complex because we want to generalize its use.
+
+- Do not hide the SUT!!!
+- Do not hide things that matter for the assertion (data, values)
+
+- It’s trade-offs: DRY/reusing (avoiding duplication) vs coupling/dependencies
+- Less code is not always better if it affects the maintainability, the readability
+    - “Four rules of simple design” (Kent Beck)
+ **/
--- exercises/writing_maintainable_tests/exercise_1/users.test.js
+++ exercises/writing_maintainable_tests/exercise_1/users.test.js
@@ -4,11 +4,16 @@ import Router from "./router.js"; // pretend this exists
 import MockDao from "./mock-dao.js"; // pretend this exists
 
 test("get 0 users OK", async () => {
+  // arrange
   const testUsers = []; // empty array of users
   const mockDao = new MockDao();
   mockDao.setUsers(testUsers);
   const router = new Router(mockDao);
+
+  // act
   const response = await router.get("/users");
+
+  // assert
   expect(response.status).toBe(200); // OK
   expect(response.headers.get("Content-Type")).toBe("application/json");
   const payload = await response.json();
@@ -21,11 +26,16 @@ test("get 0 users OK", async () => {
 });
 
 test("get 10 users OK", async () => {
+  // arrange
   const testUsers = usersForTests.slice(0, 10); // array of 10 users
   const mockDao = new MockDao();
   mockDao.setUsers(testUsers);
   const router = new Router(mockDao);
+
+  // act
   const response = await router.get("/users");
+
+  // assert
   expect(response.status).toBe(200); // OK
   expect(response.headers.get("Content-Type")).toBe("application/json");
   const payload = await response.json();
@@ -38,11 +48,16 @@ test("get 10 users OK", async () => {
 });
 
 test("get 20 users OK", async () => {
+  // arrange
   const testUsers = usersForTests.slice(0, 20); // array of 20 users
   const mockDao = new MockDao();
   mockDao.setUsers(testUsers);
   const router = new Router(mockDao);
+
+  // act
   const response = await router.get("/users");
+
+  // assert
   expect(response.status).toBe(200); // OK
   expect(response.headers.get("Content-Type")).toBe("application/json");
   const payload = await response.json();
